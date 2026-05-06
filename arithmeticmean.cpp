497a6dc0ff786b6e814d8110d6ce1d5dc9382ca1
Maryam Zahid <maryamzahid397@gmail.com>
2026-05-06 11:23:36 +0100

    Initial commit of my C++ progression code

---
+++ arithmeticmean.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std ; 
+int main(){
+    int initial_term , common_difference , n ;
+    cout<<"Enter the initial term : " ;
+    cin>>initial_term ;
+    cout<<"Enter the common difference : " ;
+    cin>>common_difference ;
+    cout<<"Enter the number of terms : " ;
+    cin>>n ;
+    for(int i = 0 ; i<n ; i++){
+        cout<<initial_term +(i*common_difference)<<endl ;
+    }
+}
