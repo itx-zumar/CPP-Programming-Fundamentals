e81d7931fb1f887b614a94c21f3bf80c4ddd27ca
Maryam Zahid <maryamzahid397@gmail.com>
2026-05-06 18:46:46 +0100

    Adding only the arithmetic progression file

--- firstcode.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-using namespace std;
-int main(){
-   // cout << "Maryam" <<"\n" << "Ali";// hehehehehheh
-   int age = 20;
-    cout << "My age is: " << age << endl;
-    return 0;
-}
--- printing.cpp
+++
@@ -1,9 +0,0 @@
-#include<iostream>
-using namespace std;
-int main(){
-    int n = 100;
-    for(int i = 0; i<=n ; i+=2){
-        cout<<i<<endl;
-    }
-    return 0;
-}
